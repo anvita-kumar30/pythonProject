37d4ce5473e87e2e444c3928f1e40c4eb97fb4e5
Anvita Kumar✨ <kumaranvita30@gmail.com>
2022-12-13 00:55:22 +0530

    Initial commit

---
+++ CommentsAndEscapeSequences.py
@@ -0,0 +1,21 @@
+# This is a single line comment
+
+"""
+This is a
+multiline
+comment
+"""
+
+#print statements
+print("Hello", end=", ")
+print("Printed on the same line")
+print("Hello","Printed on the same line") #space between the two lines
+print("next line")
+
+#Escape Sequences
+print("C:\nope")  #Inserts a new line in the text at the point
+print("C:\\nope") #Inserts a backslash character in the text at the point
+print("C:\"nope") #Inserts a double quote character in the text at that point
+print("C:\'nope") #Inserts a single quote character in the text at that point
+print("This is an escape \t sequence") #Inserts a tab in the text at that point
+print("Hello \bagain") #Inserts a backspace in the text at that point
