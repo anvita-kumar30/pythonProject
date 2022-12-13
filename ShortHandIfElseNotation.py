d0cdf0ac6ce42adf13c07b068362d0e133933b74
Anvita Kumar✨ <kumaranvita30@gmail.com>
2022-12-13 22:48:21 +0530

    init

---
+++ ShortHandIfElseNotation.py
@@ -0,0 +1,16 @@
+a = int(input("Enter a:\n"))
+b = int(input("Enter b:\n"))
+
+# if a>b: print("A is greater than B")
+
+print("B is greater than A") if a<b else print("A is greater than B")
+
+"""
+Enter a:
+12
+Enter b:
+5
+A is greater than B
+
+Process finished with exit code 0
+"""
