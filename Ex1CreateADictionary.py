b984d4dda5688bd11cd03d4cd8bc2b7acffc1dce
Anvita Kumar✨ <kumaranvita30@gmail.com>
2022-12-13 20:49:14 +0530

    init

--- Ex1CreateADictionary.py
+++ Ex1CreateADictionary.py
@@ -3,13 +3,16 @@
 print("Welcome to My Dictionary\n")
 print("Following are the words in my dictionary:-")
 print("House\nChair\nTable\nPen")
-print("\nWrite the word to get the meaning of it:")
-word = input()
+print()
+a = input("Enter the word to get the meaning of it: ")
+word = a.capitalize()
 d1 = {"House" : "The place where any person lives",
       "Chair" : "An object where a person sits",
       "Table" : "A solid object where anything can be placed",
       "Pen" : "It is a thing which can be used to write something"}
+print()
 print(d1[word])
+print(word, "means", d1[word])
 
 """
 OUTPUT:-
@@ -21,9 +24,10 @@ Chair
 Table
 Pen
 
-Write the word to get the meaning of it:
-Table
+Write the word to get the meaning of it: Table
+
 A solid object where anything can be placed
+Table means A solid object where anything can be placed
 
 Process finished with exit code 0
 """
