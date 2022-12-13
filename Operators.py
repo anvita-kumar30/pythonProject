998ce09c06363cfcd68b6897f5a4d65687641dbf
Anvita Kumar✨ <kumaranvita30@gmail.com>
2022-12-13 22:41:32 +0530

    init

---
+++ Operators.py
@@ -0,0 +1,111 @@
+# Arithmetic Operators
+print("Arithmetic  Operators")
+print("5 + 6 is ", 5+6)
+print("5 - 6 is ", 5-6)
+print("5 * 6 is ", 5*6)
+print("5 / 6 is ", 5/6)
+print("5 ** 3 is ", 5**3)  #power
+print("5 % 5 is ", 5%5)
+print("15 // 6 is ", 15//6)  #floor division operator(double divide)
+print()
+
+# Assignment Operators
+print("Assignment Operators")
+x = 5
+print(x)
+x += 7   #x = x+7
+print(x)
+x %= 7   #x = x%7
+print(x)
+print()
+
+# Comparison Operators
+print("Comparison Operators")
+i = 5
+print(i==8)
+print(i==5)
+print()
+
+
+# Logical Operators
+print("Logical Operators")
+a = True
+b = False
+print(a and a)
+print(a and b)
+print(a or a)
+print(a or b)
+print()
+
+# Identity Operators
+print("Identity Operators")
+c = 2
+d = 4
+print(c is not d)
+print(c is d)
+print()
+
+# Membership Operators
+print("Membership Operators")
+list = [3, 3, 2, 2, 39, 33, 35, 32]
+print(324 not in list)
+print(32 not in list)
+print(32 in list)
+print()
+
+# Bitwise Operators
+print("Bitwise Operators")
+# 0 - 00
+# 1 - 01
+# 2 - 10
+# 3 - 11
+
+print(0 & 2)
+print(0 & 1)
+print(0 | 1)
+print(0 | 2)
+print(0 | 3)
+
+"""
+Arithmetic  Operators
+5 + 6 is  11
+5 - 6 is  -1
+5 * 6 is  30
+5 / 6 is  0.8333333333333334
+5 ** 3 is  125
+5 % 5 is  0
+15 // 6 is  2
+
+Assignment Operators
+5
+12
+5
+
+Comparison Operators
+False
+True
+
+Logical Operators
+True
+False
+True
+True
+
+Identity Operators
+True
+False
+
+Membership Operators
+True
+False
+True
+
+Bitwise Operators
+0
+0
+1
+2
+3
+
+Process finished with exit code 0
+"""
